ed42e24333c017b47821bb8666d1d18318d2ee7f
lekhweemeng <lek.hweemeng@live.com>
2020-08-17 17:33:11 +0800

    updated and linked views and routes properly

--- teatime/views.py
+++ teatime/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('respond something here')
+    return render(request,'teatime/home.html')
