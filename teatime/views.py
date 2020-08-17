e351b85d759294bfa380c0836f6355d1e87eaff7
lekhweemeng <lek.hweemeng@live.com>
2020-08-17 18:09:07 +0800

    updated views to pick up the correct info

--- teatime/views.py
+++ teatime/views.py
@@ -3,5 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+teaBrew = [
+    {
+        'teaBrand':'Kim Guan Guan',
+        'teaAmount':'1 tablespoon',
+        'steepTime':'2mins 15sec',
+        'condenseMilk':'1.5 tablespoon',
+        'evaporatedMilk':'1.5 tablespoon',
+        'verdict':'Very Awesome!'
+    },
+    {
+        'teaBrand':'Kim Guan Guan',
+        'teaAmount':'1 tablespoon',
+        'steepTime':'2mins 15sec',
+        'condenseMilk':'1 tablespoon',
+        'evaporatedMilk':'1 tablespoon',
+        'verdict':'before this one is better. can try 1.25 spoon!'
+    }
+]
+
 def home(request):
-    return render(request,'teatime/home.html')
+    context={
+        'teaBrew' : teaBrew
+    }
+    return render(request,'teatime/home.html',context)
